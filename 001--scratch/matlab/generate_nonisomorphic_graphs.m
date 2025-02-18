8afd91b4bc2d6f4692faa5c837bf9e5ba1faec24
Andy Ackland <andy.ackland@gmail.com>
2025-02-18 18:10:32 +0000

    001--scratch/matlab/generate_nonisomorphic_graphs.m - Adding stats for number of unique graphs for range of numer of nodes (as well as a range of number of branches), printing as a matrix with totals. * tested OK. {{{ >> generate_nonisomorphic_graphs( 2, 7, 1, 6 ) Number of unique graphs with 2 to 7 nodes and 1 to 6 edges: N\E	1	2	3	4	5	6	Tot 2	1	0	0	0	0	0	1 3	0	1	1	0	0	0	2 4	0	0	2	2	1	1	6 5	0	0	0	3	5	5	13 6	0	0	0	0	6	13	19 7	0	0	0	0	0	11	11 Tot	1	1	3	5	12	30	52 }}} * execution time is 44 seconds (36 seconds executing isisomorphic). See #1 (Scratch the surface with some MATLAB scripts)

--- 001--scratch/matlab/generate_nonisomorphic_graphs.m
+++ 001--scratch/matlab/generate_nonisomorphic_graphs.m
@@ -1,79 +1,96 @@
-function generate_nonisomorphic_graphs ( N, M1, M2, showFigures )
-    % This function generates non-isomorphic connected simple graphs with N nodes and M1 to M2 edges
+function generate_nonisomorphic_graphs ( N1, N2, M1, M2, showFigures )
+    % This function generates non-isomorphic connected simple graphs with N1 to N2 nodes and M1 to M2 edges
     % If showFigures is not provided, default to false
-    if nargin < 4
+    if nargin < 5
         showFigures = false;
     end
 
     % Initialize a matrix to store the counts of unique graphs
-    unique_graph_counts = zeros ( 1, M2 - M1 + 1 );
+    unique_graph_counts = zeros ( N2 - N1 + 1, M2 - M1 + 1 );
 
-    % Generate graphs for each number of edges from M1 to M2
-    for M = M1:M2
-        % Generate all possible combinations of edges
-        edges = nchoosek ( 1:N, 2 );
-        num_edges = size ( edges, 1 );
-        if M > num_edges
-            error ( 'Too many edges for the given number of nodes' );
-        end
+    % Generate graphs for each number of nodes from N1 to N2
+    for N = N1:N2
+        % Generate graphs for each number of edges from M1 to M2
+        for M = M1:M2
+            % Generate all possible combinations of edges
+            edges = nchoosek ( 1:N, 2 );
+            num_edges = size ( edges, 1 );
 
-        % Initialize a cell array to store the unique graphs
-        unique_graphs = {};
+            % Initialize a cell array to store the unique graphs
+            unique_graphs = {};
 
-        % Iterate over all possible subsets of edges
-        combinations = nchoosek ( 1:num_edges, M );
-        for i = 1:size ( combinations, 1 )
-            edge_subset = edges ( combinations ( i, : ), : );
+            if M <= num_edges
+                % Iterate over all possible subsets of edges
+                combinations = nchoosek ( 1:num_edges, M );
+                for i = 1:size ( combinations, 1 )
+                    edge_subset = edges ( combinations ( i, : ), : );
 
-            % Create the adjacency matrix
-            adj_matrix = zeros ( N );
-            for j = 1:M
-                adj_matrix ( edge_subset ( j, 1 ), edge_subset ( j, 2 ) ) = 1;
-                adj_matrix ( edge_subset ( j, 2 ), edge_subset ( j, 1 ) ) = 1;
-            end
+                    % Create the adjacency matrix
+                    adj_matrix = zeros ( N );
+                    for j = 1:M
+                        adj_matrix ( edge_subset ( j, 1 ), edge_subset ( j, 2 ) ) = 1;
+                        adj_matrix ( edge_subset ( j, 2 ), edge_subset ( j, 1 ) ) = 1;
+                    end
 
-            % Check if the graph is connected
-            G = graph ( adj_matrix );
-            if all ( conncomp ( G ) == 1 )
-                % Check if the graph is isomorphic to any existing graph in unique_graphs
-                is_unique = true;
-                for k = 1:length ( unique_graphs )
-                    if isisomorphic ( G, unique_graphs { k } )
-                        is_unique = false;
-                        break;
+                    % Check if the graph is connected
+                    G = graph ( adj_matrix );
+                    if all ( conncomp ( G ) == 1 )
+                        % Check if the graph is isomorphic to any existing graph in unique_graphs
+                        is_unique = true;
+                        for k = 1:length ( unique_graphs )
+                            if isisomorphic ( G, unique_graphs { k } )
+                                is_unique = false;
+                                break;
+                            end
+                        end
+
+                        % Add the graph to unique_graphs if it's unique
+                        if is_unique
+                            unique_graphs { end + 1 } = G;
+                        end
                     end
                 end
-
-                % Add the graph to unique_graphs if it's unique
-                if is_unique
-                    unique_graphs { end + 1 } = G;
-                end
             end
-        end
 
-        % Store the count of unique graphs
-        unique_graph_counts ( M - M1 + 1 ) = length ( unique_graphs );
+            % Store the count of unique graphs
+            unique_graph_counts ( N - N1 + 1, M - M1 + 1 ) = length ( unique_graphs );
 
-        % Display the unique graphs if showFigures is true
-        if showFigures
-            for i = 1:length ( unique_graphs )
-                figure;
-                plot ( unique_graphs { i } );
-                title ( sprintf ( 'Graph %d with %d edges', i, M ) );
+            % Display the unique graphs if showFigures is true
+            if showFigures
+                for i = 1:length ( unique_graphs )
+                    figure;
+                    plot ( unique_graphs { i } );
+                    title ( sprintf ( 'Graph %d with %d nodes and %d edges', i, N, M ) );
+                end
             end
         end
     end
 
+    % Calculate column totals
+    column_totals = sum ( unique_graph_counts, 1 );
+
+    % Calculate row totals
+    row_totals = sum ( unique_graph_counts, 2 );
+
     % Print the statistics
-    fprintf ( 'Number of unique graphs with %d nodes and %d to %d edges:\n', N, M1, M2 );
-    fprintf ( 'Edges\t' );
+    fprintf ( 'Number of unique graphs with %d to %d nodes and %d to %d edges:\n', N1, N2, M1, M2 );
+    fprintf ( 'N\\E\t' );
     for M = M1:M2
         fprintf ( '%d\t', M );
     end
-    fprintf ( 'Total\n' );
-    fprintf ( 'Count\t' );
-    for count = unique_graph_counts
-        fprintf ( '%d\t', count );
+    fprintf ( 'Tot\n' );
+    for N = N1:N2
+        fprintf ( '%d\t', N );
+        for M = M1:M2
+            fprintf ( '%d\t', unique_graph_counts ( N - N1 + 1, M - M1 + 1 ) );
+        end
+        fprintf ( '%d\n', row_totals ( N - N1 + 1 ) );
     end
-    fprintf ( '%d\n', sum ( unique_graph_counts ) );
+
+    % Print column totals
+    fprintf ( 'Tot\t' );
+    for total = column_totals
+        fprintf ( '%d\t', total );
+    end
+    fprintf ( '%d\n', sum ( column_totals ) );
 end
