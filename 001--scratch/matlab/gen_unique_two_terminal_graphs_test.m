dbf88054f021b66240a00d40bc680b7f849d404f
Andy Ackland <andy.ackland@gmail.com>
2025-02-19 21:18:56 +0000

    001--scratch/matlab/gen_unique_two_terminal_graphs.m * Nodes 1 and 2 are treated as terminal nodes, and node labels are used in isomorphism tests. * Tested OK 001--scratch/matlab/gen_unique_two_terminal_graphs_test.m * The number of nonisomorphic graphs increases dramatically when two nodes are labelled as terminals and isomorphism excludes differences in labelling. * The total number for graphs for connected graphs of 2 to 6 edges goes from 22 -> 104, and the generation time goes from 1 -> 33 sec * The increase in execution time is 93% under the MATLAB built-in function `graph.isisomorphic()`.
    Compare with
    
    {{{
    >> clear; generate_nonisomorphic_graphs_test
    Number of unique graphs with 2 to 6 nodes and 1 to 5 edges:
    N\E	1	2	3	4	5	Tot
    2	1	0	0	0	0	1
    3	0	1	1	0	0	2
    4	0	0	2	2	1	5
    5	0	0	0	3	5	8
    6	0	0	0	0	6	6
    Tot	1	1	3	5	12	22
    Elapsed time is 1.062968 seconds.
    }}}
    {{{
    >> clear; gen_unique_two_terminal_graphs_test
    Number of unique graphs with 2 to 6 nodes and 1 to 5 edges:
    N\E	1	2	3	4	5	Tot
    2	1	0	0	0	0	1
    3	0	2	1	0	0	3
    4	0	0	6	6	3	15
    5	0	0	0	15	27	42
    6	0	0	0	0	43	43
    Tot	1	2	7	21	73	104
    Elapsed time is 33.498619 seconds.
    >>
    }}}
    See #1 (Scratch the surface with some MATLAB scripts)

--- 001--scratch/matlab/gen_unique_two_terminal_graphs_test.m
+++ 001--scratch/matlab/gen_unique_two_terminal_graphs_test.m
@@ -1,5 +1,5 @@
 M1 = 1; M2 = 5;      % min, max number of edges
 N1 = 2; N2 = M2 + 1; % min, max number of nodes
 tic
-generate_nonisomorphic_graphs( N1, N2, M1, M2 )
+gen_unique_two_terminal_graphs( N1, N2, M1, M2 )
 toc
